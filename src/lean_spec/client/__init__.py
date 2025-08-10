ac41ad4de5a2a19f876dda340677d68e48cbc148
unnawut <unnawut@unnawut.com>
2025-08-10 14:25:39 +0700

    docs: add change comment

--- src/lean_spec/client/__init__.py
+++ src/lean_spec/client/__init__.py
@@ -5,4 +5,5 @@ Key differences from 3SF-mini.py:
 - Using `U64` instead of native `int` for all fields
 - Using `Bytes32` instead of native `str` for all fields
 - Combined `*_root` and `*_slot` pairs into a single `Checkpoint` field
+- Removed optionals from `Block`
 """
