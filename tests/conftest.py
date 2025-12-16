8dd3d11b1671e6559451f5bd1226133835bf7494
unnawut <unnawut@unnawut.com>
2025-12-16 16:41:48 +0700

    fix: remove LEAN_ENV default from conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,10 +4,6 @@ import os
 
 from hypothesis import settings
 
-# Set LEAN_ENV=test before importing any modules that depend on it
-if "LEAN_ENV" not in os.environ:
-    os.environ["LEAN_ENV"] = "test"
-
 import lean_spec.subspecs.xmss as xmss
 
 # Create a profile named "no_deadline" with deadline disabled.
